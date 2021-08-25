683d3a9bcdf207545cea81471d8aba77fe5dc170
noampl <noampl74@gmail.com>
2021-08-25 11:53:44 +0300

    add the Branch, Address and OpeningHour models

---
+++ Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_development_course.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Street { get; set; }
+
+        [Required]
+        [RegularExpression("([1-9][0-9]*)"), ErrorMessage = "Characters are not allowed.")]
+        public int BuildingNumber { get; set; }
+    }
+}

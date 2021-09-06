e1536c3ad47a141289422e050ff3813037303eba
Frishman <frishman.itay@gmail.com>
2021-09-06 12:14:43 +0300

    Added OrderModels, ProductModels and User model (#12)
    * Added OrderModels, ProductModels and User model
    
    * Update Models/OrderModels/Order.cs
    
    changed OrderID to id
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    
    * Update Models/OrderModels/OrderItem.cs
    
    changed OrderItemID to Id
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    
    * Update Models/ProductModels/Category.cs
    
    changed CategoryID to id
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    
    * Update Models/ProductModels/Product.cs
    
    changed ProductID to Id
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    
    * Update Models/ProductModels/ProductType.cs
    
    changed ProductTypeID to Id
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    
    * Update Models/User.cs
    
    changed UserID to Id
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    
    * added ProductSize enum and deleted the image from the db.
    
    * Added display and validation to TotalPrice
    
    * fixed column in OrderItem to Display
    
    * Remove empty migration
    
    * Address comments
    
    Co-authored-by: Bar Admoni <bar6020677@gmail.com>
    Co-authored-by: Bar Admoni <badmoni@cisco.com>

---
+++ Models/OrderModels/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_development_course.Models.OrderModels
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public IEnumerable<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using web_development_course.Models.OrderModels;
+
+namespace web_development_course.Models
+{
+    public enum UserLevel
+    {
+        Client,
+        Editor,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public UserLevel UserType { get; set; } = UserLevel.Client;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public int OrderID { get; set; }
+
+        public Order Cart { get; set; }
+
+        public IEnumerable<Order> Orders { get; set; }
+
+    }
+}

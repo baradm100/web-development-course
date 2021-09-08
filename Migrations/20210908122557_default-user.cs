ab8ac86647c68c996872395aae8b5c57eb324948
Itay <Frishman.itay@gmail.com>
2021-09-08 15:30:33 +0300

    added default admin user and corrected create new user page

---
+++ Migrations/20210908122557_default-user.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace web_development_course.Migrations
+{
+    public partial class defaultuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "UserType",
+                value: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "UserType",
+                value: 0);
+        }
+    }
+}

da53790e667b3f0e83cf2097d3d651276b531662
Bar Admoni <bar6020677@gmail.com>
2021-09-22 09:05:36 +0300

    Adding Categories To Navbar (#26)
    * Adding Categories To Navbar
    
    Adding the categories to the navbar, each category will have it's  sub catagories and a link to all.
    
    * Moved to partial
    
    * Making things pretty

---
+++ Data/CategoryTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using web_development_course.Models;
+
+namespace web_development_course.Data
+{
+    public class CategoryTree
+    {
+        public Category Category { get; set; }
+
+        public List<Category> SubCategories { get; set; }
+    }
+}
---
+++ Models/ProductModels/CategoryModelService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using web_development_course.Data;
+
+namespace web_development_course.Models.ProductModels
+{
+    public class CategoryModelService
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public CategoryModelService(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public List<CategoryTree> GetCategoryTrees()
+        {
+            var mainCatagories = _context.Category.Where(c => c.ParentCategoryId == null).ToList<Category>();
+            List<CategoryTree> categoryTrees = new List<CategoryTree>();
+
+            foreach (Category mainCategory in mainCatagories)
+            {
+                List<Category> subCatagories = _context.Category.Where(c => c.ParentCategoryId == mainCategory.Id).ToList<Category>();
+                categoryTrees.Add(new CategoryTree { Category = mainCategory, SubCategories = subCatagories });
+            }
+
+
+            return categoryTrees;
+        }
+    }
+}

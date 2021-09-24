ebdbe63dfcd2477fcb03e0298f57d3fac72df419
Noam Peleg <49092985+noampl@users.noreply.github.com>
2021-09-24 10:46:39 +0300

    Create BranchControllers (#17)
    * Create BranchControllers
    
    * mid work,
    trying to figure how to connect the google maps api
    
    * mid work,
    get the help of frishman
    
    * Fixed Create new branch
    
    * 1. fix google maps, now it shows the points
    2. when press show in map, the map show the spesific store
    3. create and edit now work
    4. fix the openhour model to hold fk of branch
    
    open bugs:
    yet it's not possile to edit or create the OpeningHours from the Branch views
    
    * All working well for now,
    thigs to do later:
    1. figure how to put th .js file outside the cshtml
    2. change the time to somethong pretier
    3. when edit or creating make sure there is no objet with the same properties
    
    * fix cr comment:
    1. Move thr js fiel to the same file
    2. delete unneeded controlles and vies
    3. check for login user before shoing all the options
    
    Co-authored-by: Itay <Frishman.itay@gmail.com>

--- Models/Address.cs
+++ Models/Address.cs
@@ -20,7 +20,19 @@ namespace web_development_course.Models
         public string Street { get; set; }
 
         [Required]
+        [Display(Name = "Building Number")]
         [Range(Consts.FIRST_BUILDING_NUM, Consts.MAX_BUILDING_NUM, ErrorMessage = Consts.ONLY_DIGITS_ERROR)]
         public int BuildingNumber { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        public override string ToString()
+        {
+            return Street + " St." + BuildingNumber + " " + City; 
+        }
     }
 }

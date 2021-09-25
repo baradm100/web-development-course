8a326163b780a70ea1439ed0228b79ddc6d037ba
Frishman <frishman.itay@gmail.com>
2021-09-25 20:29:12 +0300

    Currency change works

---
+++ Controllers/UtilsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_development_course.WebServices;
+
+namespace web_development_course.Controllers
+{
+    public class UtilsController : Controller
+    {
+
+        private CurrencyConverter ct;
+
+        // GET: /currency/json
+        [HttpGet]
+        [Route("currency/json")]
+        public IActionResult GetCurrency(string currency)
+        {
+            // dollar is equal to 1, its the default value in app
+            double currencyValue = 1;
+            if (ct == null)
+            {
+                ct = new CurrencyConverter();
+                ct.initCurrencyRates();
+            }
+            if (currency == "GBP")
+                currencyValue = ct.Gbp;
+            else if (currency == "EUR")
+                currencyValue = ct.Eur;
+            else if (currency == "ILS")
+                currencyValue = ct.Ils;
+            return Json(new { success = true, value = currencyValue });
+        }
+    }
+}
---
+++ wwwroot/js/currency.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+    var currencisSign = {
+        "USD": 1,
+        "ILS": 2,
+        "EUR": 3,
+        "GBP": 4,
+    }
+    $currencyButton = $(".currencyBtn");
+    var currentCurrency = document.cookie["currency"];
+
+    $currencyButton.click(function () {
+        var selectedCurrency = $(this).children("i").attr("id");
+        if (currentCurrency == selectedCurrency)
+            return true
+        $.ajax({
+            url: "/currency/json?currency=" + selectedCurrency,
+            type: 'GET',
+            dataType: 'json',
+            success: function (result) {
+                if (result.success) {
+                    document.cookie = "currencySign=" + currencisSign[selectedCurrency];
+                    document.cookie = "currency=" + result.value;
+                    return true;
+                }
+                document.cookie = "currency=" + 1 + ",currencySign=" + currencisSign["USD"];
+            },
+            error: function (result) {
+                console.log(result);
+                document.cookie = "currency=" + 1 + ",currencySign=" + currencisSign["USD"];
+                return false;
+            },
+            complete: function () {
+                document.location.reload();
+            },
+            timeout: 5000
+        });
+    })
+});

7a53e47250d72a176e46f5211006d8a866afaaef
Noam Peleg <49092985+noampl@users.noreply.github.com>
2021-11-06 13:49:56 +0200

    Add phone and confrm password to register screen (#103)
    make it more scure
    logedin the register user after registerition

---
+++ wwwroot/js/register/register.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    $("#register").click(function () {
+        $("#confirm-error").addClass("d-none")
+
+        if (validPassword()) {
+            createUser()
+            return true;
+        }
+        else {
+            $("#confirm-error").removeClass("d-none")
+            return false;
+        }
+    })
+
+
+    function validPassword() {
+
+        return ($("#Confirm-Password").val() === $("#password").val())
+    }
+
+    function createUser() {
+        console.log("create user")
+    }
+
+
+
+});

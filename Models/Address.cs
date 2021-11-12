4ccb87377e5d3718f394cd6d5f03f73df6509342
Noam Peleg <49092985+noampl@users.noreply.github.com>
2021-11-12 17:06:05 +0200

    Fix Branch modul: (#170)
    1. edit / create branch
    2. mobule view
    3. scrolling brnaches
    4. navigate to branch in mobile view

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using web_development_course.Common;
 
 namespace web_development_course.Models

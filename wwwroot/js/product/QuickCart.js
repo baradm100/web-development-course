829ae5abf70be6e56295e469b1d206012702a3b7
Bar Admoni <badmoni@cisco.com>
2021-11-12 12:14:14 +0200

    Refresh checkout modal after adding an item to cart

--- wwwroot/js/product/QuickCart.js
+++ wwwroot/js/product/QuickCart.js
@@ -169,6 +169,9 @@ function openCart() {
 }
 
 function updateCart(shouldOpen = true) {
+  // Update the cart for the checkout modal
+  getCart();
+
   $.ajax({
     type: "GET",
     url: "/Orders/GetCart",
